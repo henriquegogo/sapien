2b9898774b048e07a3e0aa201332c2d364a007e0
Henrique Gogó <henriquegogo@gmail.com>
2025-07-17 14:06:36 -0300

    Generate query with spaces and break lines

--- generate-query.sql
+++ generate-query.sql
@@ -1,20 +1,41 @@
 BEGIN TRANSACTION;
 
 SELECT
-printf("CREATE VIEW IF NOT EXISTS %s_view AS SELECT id, json_object('id',id,%s) AS _json FROM %s;
-", tbl.name, group_concat(quote(col.name) || ',' || col.name), tbl.name) ||
+printf("CREATE VIEW IF NOT EXISTS %s_view AS
+SELECT id, json_object(
+  'id', id,%s
+) AS _json
+FROM %s;
+", tbl.name, group_concat("
+  " || quote(col.name) || ', ' || col.name), tbl.name) ||
 
 printf("
-CREATE TRIGGER IF NOT EXISTS post_%s INSTEAD OF INSERT ON %s_view BEGIN INSERT INTO %s VALUES (NULL,%s); END;
-", tbl.name, tbl.name, tbl.name, group_concat("json_extract(NEW._json,'$." || col.name || "')")) ||
+CREATE TRIGGER IF NOT EXISTS post_%s
+INSTEAD OF INSERT ON %s_view
+BEGIN
+  INSERT INTO %s VALUES (NULL,%s);
+END;
+", tbl.name, tbl.name, tbl.name, group_concat("
+    json_extract(NEW._json, '$." || col.name || "')") || "
+  ") ||
 
 printf("
-CREATE TRIGGER IF NOT EXISTS put_%s INSTEAD OF UPDATE ON %s_view BEGIN UPDATE %s SET %s WHERE id=NEW.id; END;
+CREATE TRIGGER IF NOT EXISTS put_%s
+INSTEAD OF UPDATE ON %s_view
+BEGIN
+  UPDATE %s SET %s
+  WHERE id = NEW.id;
+END;
 ", tbl.name, tbl.name, tbl.name,
-group_concat(col.name || "=json_extract(NEW._json,'$." || col.name || "')")) ||
+group_concat("
+    " || col.name || " = json_extract(NEW._json, '$." || col.name || "')")) ||
 
 printf("
-CREATE TRIGGER IF NOT EXISTS delete_%s INSTEAD OF DELETE ON %s_view BEGIN DELETE FROM %s WHERE id=OLD.id; END;
+CREATE TRIGGER IF NOT EXISTS delete_%s
+INSTEAD OF DELETE ON %s_view
+BEGIN
+  DELETE FROM %s WHERE id = OLD.id;
+END;
 ", tbl.name, tbl.name, tbl.name)
 
 FROM sqlite_master AS tbl, pragma_table_info(tbl.name) AS col
